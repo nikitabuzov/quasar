a9cb565d791b1246eb5a554045af0e55673a5b67
Nikita Buzov <nikitabuzov@gmail.com>
2020-11-22 19:30:21 -0800

    integrated SafeMath

--- contracts/CapitalPool.sol
+++ contracts/CapitalPool.sol
@@ -1,8 +1,10 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract CapitalPool is Ownable {
+    using SafeMath for uint256;
 
     // State variables
     uint256 public coveragePrice;
@@ -20,9 +22,9 @@ contract CapitalPool is Ownable {
         uint256 coverAmount;
     }
     // Modifiers
-    modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
-    modifier validPeriod(uint _period) {require(_period >= 1209600 && _period <= 31536000); _;}
-    modifier coverAvailable(uint _coverAmount) {require(_coverAmount <= availableCapital); _;}
+    modifier paidEnough(uint256 _price) { require(msg.value >= _price); _;}
+    modifier validPeriod(uint256 _period) {require(_period >= 1209600 && _period <= 31536000); _;}
+    modifier coverAvailable(uint256 _coverAmount) {require(_coverAmount <= availableCapital); _;}
 
     // Events
     event logCoverPurchase(address indexed buyer);
@@ -44,18 +46,18 @@ contract CapitalPool is Ownable {
 
     function buyCoverage(uint256 _coverPeriod, uint256 _coverAmount)
         external payable
-        paidEnough(_coverAmount*coveragePrice/100*_coverPeriod/31536000)
+        paidEnough(_coverAmount.mul(coveragePrice).mul(_coverPeriod).div(100).div(31536000))
         validPeriod(_coverPeriod)
         coverAvailable(_coverAmount)
         returns(bool)
     {
         emit logCoverPurchase(msg.sender);
         coverages[msg.sender].id = coverCount;
-        coverages[msg.sender].expirationTime = now + _coverPeriod;
+        coverages[msg.sender].expirationTime = now.add(_coverPeriod);
         coverages[msg.sender].coverAmount = _coverAmount;
         coverCount++;
-        mcr = mcr + _coverAmount;
-        availableCapital = capitalPool - mcr;
+        mcr = mcr.add(_coverAmount);
+        availableCapital = capitalPool.sub(mcr);
 
         return true;
     }
@@ -65,8 +67,8 @@ contract CapitalPool is Ownable {
         returns(bool)
     {
         emit logCapitalDeposited(msg.sender);
-        capitalPool = capitalPool + msg.value;
-        availableCapital = capitalPool - mcr;
+        capitalPool = capitalPool.add(msg.value);
+        availableCapital = capitalPool.sub(mcr);
         providers[msg.sender] = true;
         balanceOf[msg.sender] = msg.value;
 
