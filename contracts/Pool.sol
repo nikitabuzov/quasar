ad5846c3c33d6b452a07468368297ea28ac2d71e
Nikita Buzov <nikitabuzov@gmail.com>
2020-12-28 21:32:07 +0300

    added NatSpec comments

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -8,70 +8,83 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./QuasarToken.sol";
 
-
+/// @title Quasar: capital pool that facilitates mutual-like risk sharing 
+/// @author Nikita S. Buzov
+/// @notice This contract lets liquidity (coverage) providers to
+///         deposit funds and earn fees + native token rewards,
+///         while users can buy coverage against the pool
 contract Pool is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     /* ========== STATE VARIABLES ========== */
 
-    QuasarToken public rewardsToken;
+    QuasarToken public rewardsToken; // native token for rewards
     bool private stopped = false; // circuit breaker
-    // rewards distribution
+
+    // rewards distribution variables / dictionaries :
     uint256 public periodFinish = 0;
     uint256 public rewardRate = 0;
     uint256 public rewardsDuration = 60 days;
     uint256 public lastUpdateTime;
     uint256 public rewardPerEthStored;
-
     mapping(address => uint256) public userRewardPerEthPaid;
     mapping(address => uint256) public rewards;
 
-    // coverage plans
-    uint256 public coveragePrice;
-    uint256 public coverCount;
+    // coverage plan variables:
+    uint256 public coveragePrice; // price as percentage of ETH amount covered for 365 days
+    uint256 public coverCount; // id number is simply a count
     uint256 public mcr; // Minimum Capital Requirement (pool must be able to cover all possible claims)
-    uint256 public availableCapital; // how much of coverage can the pool deliver ( = pool - mcr)
+    uint256 public availableCapital; // how much of coverage can the pool deliver, equals to (pool - mcr)
 
-    mapping (address => Coverage) public coverageOf;
-    mapping (uint256 => bool)  public openClaims;
-    mapping (uint256 => address) public buyerOf;
+    mapping (address => Coverage) public coverageOf; // assign address to the purchased coverage plan
+    mapping (uint256 => bool)  public openClaims; // track open claims
+    mapping (uint256 => address) public buyerOf; // mapping of a coverage plan id to the user
     struct Coverage {
         uint256 id;
         uint256 expirationTime;
         uint256 coverAmount;
     }
 
-    // pool balance tracking
-    uint256 public _totalSupply; // capital pool balance
-    mapping(address => uint256) private _balances; // balances of coverage providers
+    // capital pool variables:
+    uint256 public _totalSupply; // capital pool balance (amount of ETH this contract has)
+    mapping(address => uint256) private _balances; // balances of coverage (i.e. liquidity) providers
 
     /* ========== CONSTRUCTOR ========== */
 
     constructor() public {
-        rewardsToken = new QuasarToken();
-        rewardsToken.mint(address(this),1000000);
-        notifyRewardAmount(1000000);
+        rewardsToken = new QuasarToken();           // assign new token as the rewards token
+        rewardsToken.mint(address(this),1000000);   // mint 1000000 QSR to this contract
+        notifyRewardAmount(1000000);                // allocate all the tokens for reward distribution
         coverCount = 0;
-        coveragePrice = 2;
+        coveragePrice = 2;                          // this set the cover price to 2%
         mcr = 0;
         availableCapital = 0;
     }
 
     /* ========== VIEWS ========== */
 
+    /// @notice View the total supply of ETH deposited into this contract
+    /// @return Amount in Wei
     function totalSupply() external view returns (uint256) {
         return _totalSupply;
     }
 
+    /// @notice View the ETH balance of the capital provider account
+    /// @param account that deposited ETH
+    /// @return Balance in Wei
     function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
     }
 
+    /// @notice View the last time the QSR rewards were applied
+    /// @return the UNIX timestamp
     function lastTimeRewardApplicable() public view returns (uint256) {
         return Math.min(block.timestamp, periodFinish);
     }
 
+    /// @notice View the QSR reward amount per ETH deposited
+    /// @return Amount in QSR tokens
     function rewardPerEth() public view returns (uint256) {
         if (_totalSupply == 0) {
             return rewardPerEthStored;
@@ -82,16 +95,26 @@ contract Pool is Ownable, ReentrancyGuard {
             );
     }
 
+    /// @notice View the earned QSR rewards
+    /// @param account that earned the rewards
+    /// @return the QSR balance of the account
     function earned(address account) public view returns (uint256) {
         return _balances[account].mul(rewardPerEth().sub(userRewardPerEthPaid[account])).div(1e18).add(rewards[account]);
     }
 
+    /// @notice View the total amount of rewards
+    /// @return amount of QSR rewards for the full distribution period
     function getRewardForDuration() external view returns (uint256) {
         return rewardRate.mul(rewardsDuration);
     }
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
+    /// @notice Purchase coverage for specified ETH amount and time period
+    /// @dev This function call must be made with an ETH payment in the amount of `coveragePrice`% / year
+    /// @param _coverPeriod Period for which the coverage plan is active (in units of seconds)
+    /// @param _coverAmount Amount of ETH to be covered by the plan (in units of Wei)
+    /// @return true / false based on success of the function call
     function buyCoverage(uint256 _coverPeriod, uint256 _coverAmount)
         external payable
         stopInEmergency
@@ -111,6 +134,8 @@ contract Pool is Ownable, ReentrancyGuard {
         return true;
     }
 
+    /// @notice Deposit ETH to become coverage (liquidity) provider
+    /// @dev This function call should be made with an ETH payment in any amount, no parameters
     function deposit()
         external payable
         nonReentrant
@@ -124,6 +149,9 @@ contract Pool is Ownable, ReentrancyGuard {
         emit Deposited(msg.sender,msg.value);
     }
 
+    /// @notice Withdraw ETH if you are a coverage (liquidity) provider
+    /// @dev If you would like to withdraw all the funds, then just call exit() function
+    /// @param amount Amount of ETH to be withdrawn (in units of Wei)
     function withdraw(uint256 amount)
         public
         nonReentrant
@@ -142,6 +170,8 @@ contract Pool is Ownable, ReentrancyGuard {
         
     }
 
+    /// @notice Claim QSR rewards if you're providing capital
+    /// @dev In this version the rewards distribution doesn't seem to work, though the function calls are successful
     function getReward()
         public
         nonReentrant
@@ -156,6 +186,7 @@ contract Pool is Ownable, ReentrancyGuard {
         }
     }
 
+    /// @notice Withdraw all ETH and all QSR rewards if you are a coverage (liquidity) provider
     function exit()
         external
     {
@@ -163,6 +194,9 @@ contract Pool is Ownable, ReentrancyGuard {
         getReward();
     }
 
+    /// @notice Open claim if you own a plan and something happend
+    /// @dev This function call simply sends a message to the contract owner and creates and open claim
+    /// @param statement Message to the contract owner with a statement
     function openClaim(string calldata statement)
         external
         validCover(msg.sender)
@@ -172,6 +206,13 @@ contract Pool is Ownable, ReentrancyGuard {
         emit ClaimOpened(msg.sender, coverageOf[msg.sender].id, statement);
     }
 
+
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /// @notice Resolves claim and sends a payout if the claim is valid
+    /// @param coverageID The coverage ID number of the claim to be resolved by the contract owner
+    /// @param decision Owner's decision for the claim
     function resolveClaim(uint256 coverageID, bool decision)
         public
         onlyOwner
@@ -190,8 +231,8 @@ contract Pool is Ownable, ReentrancyGuard {
         emit ClaimResolved(coverageID, decision);
     }
 
-    /* ========== RESTRICTED FUNCTIONS ========== */
-
+    /// @notice Sets the coverage price
+    /// @param _coveragePrice The coverage price as percentage of ETH amount covered for 365 days
     function setCoveragePrice(uint256 _coveragePrice)
         public
         onlyOwner
@@ -200,6 +241,8 @@ contract Pool is Ownable, ReentrancyGuard {
         emit CoverPriceUpdated(coveragePrice);
     }
 
+    /// @notice Circuit breaker, pauses the contract capital flows
+    /// @dev Contract owner can turn off all ETH and QSR capital flows from/to this contract
     function toggleContractActive()
         public
         onlyOwner
@@ -207,6 +250,9 @@ contract Pool is Ownable, ReentrancyGuard {
         stopped = !stopped;
     }
 
+
+    /// @notice Allocates the minted QSR tokens as reward
+    /// @param reward Number of reward tokens
     function notifyRewardAmount(uint256 reward)
         internal
         updateReward(address(0))
